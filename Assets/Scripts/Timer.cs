8f0d6c34f599be228e4d5548154f2384fa0d6bec
Nagiphaaa <90415429+Nagiphaaa@users.noreply.github.com>
2025-11-21 19:58:48 +0100

    Timer: Add documentation

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// Class handling timers.
+/// </summary>
 public class Timer : MonoBehaviour
 {
     private float duration;
@@ -12,12 +15,20 @@ public class Timer : MonoBehaviour
 
     public bool IsRunning => timerRoutine != null;
 
+    /// <summary>
+    /// Initialise a timer with a given duration and an UnityEvent which shall be called once the timer ends.
+    /// </summary>
+    /// <param name="duration">Duration of the timer.</param>
+    /// <param name="onFinish">Event to be called upon the timer's completion.</param>
     public void Init(float duration, UnityEvent onFinish)
     {
         this.duration = duration;
         this.onFinish = onFinish;
     }
 
+    /// <summary>
+    /// Start the timer.
+    /// </summary>
     public void StartTimer()
     {
         if (timerRoutine != null)
@@ -26,6 +37,9 @@ public class Timer : MonoBehaviour
         timerRoutine = StartCoroutine(RunTimer());
     }
 
+    /// <summary>
+    /// Stop the timer.
+    /// </summary>
     public void StopTimer()
     {
         if (timerRoutine != null)
@@ -39,6 +53,10 @@ public class Timer : MonoBehaviour
     public void Resume() => isPaused = false;
     public float GetTimeRemaining() => Mathf.Max(0f, timeRemaining);
 
+    /// <summary>
+    /// Run the timer
+    /// </summary>
+    /// <returns>null</returns>
     private IEnumerator RunTimer()
     {
         timeRemaining = duration;
