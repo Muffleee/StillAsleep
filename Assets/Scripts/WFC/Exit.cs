d5eafc954e82ff40d1f27e7a48acff263ab16a22
Nagiphaaa <90415429+Nagiphaaa@users.noreply.github.com>
2025-11-21 19:48:47 +0100

    Exit: Add documentation

--- Assets/Scripts/WFC/Exit.cs
+++ Assets/Scripts/WFC/Exit.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class describing the game's exit
+/// </summary>
 public class Exit
 {
     public GridObj gridObj;
     public int growthIndex;
 
+    /// <summary>
+    /// Create the Exit from a GridObj and a growthIndex
+    /// </summary>
+    /// <param name="gridObj">GridObj at which the exit should be</param>
+    /// <param name="growthIndex"></param>
     public Exit(GridObj gridObj, int growthIndex)
     {
         this.gridObj = gridObj;
