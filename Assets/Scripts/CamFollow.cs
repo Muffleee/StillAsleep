fa847c1c0cda62135db9e95960cc2e8d11674e53
Nagiphaaa <90415429+Nagiphaaa@users.noreply.github.com>
2025-11-21 19:52:41 +0100

    CamFollow: Added comments & minor clean-up

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Attached to a camera, this class ensures the camera follows the player's position.
+/// </summary>
 public class CamFollow : MonoBehaviour
 {
     [SerializeField] private Vector3 camOffset = new Vector3(0f, 7f, -4.5f);
     [SerializeField] private Transform target;
     private void Update()
     {
-        this.transform.position = target.position + camOffset;
+        transform.position = target.position + camOffset;
     }
 }
