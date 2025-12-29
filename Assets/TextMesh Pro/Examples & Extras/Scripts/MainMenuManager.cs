044ceb7b0f57c2f62f116b0c7621add9d4d8942c
Babachu38 <ngbabachu@gmail.com>
2025-12-29 03:37:23 +0100

    Added Menu

---
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/MainMenuManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+namespace MainMenu
+{
+    
+    public class MainMenu : MonoBehaviour
+    {
+
+        [SerializeField] private GameObject MainMenuPanel;
+        [SerializeField] private TMP_Text gameTitleText;
+        [SerializeField] private Button startButton;
+        [SerializeField] private Button quitButton;
+
+        [SerializeField] private string gameSceneName;
+        void Start()
+        {
+            ShowMainMenu();
+
+            if (this.startButton != null)
+                startButton.onClick.AddListener(StartGame);
+
+            if (this.quitButton != null)
+                quitButton.onClick.AddListener(QuitGame);
+        }
+
+        public void ShowMainMenu()
+        {
+            if(this.MainMenuPanel != null)
+            {
+                MainMenuPanel.SetActive(true);
+            }
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene(gameSceneName);
+        }
+
+        private void QuitGame()
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
