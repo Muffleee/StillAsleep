8e8e53054132ab6db2e55bd2d0ee451e96b94b3c
Lucida22 <105169842+Lucida22@users.noreply.github.com>
2026-02-03 19:49:04 +0100

    implement music

--- Assets/Scripts/AudioManager/AudioManager.cs
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
-    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip tilePlacing;
     [SerializeField] private AudioClip buttonClick;
+    [SerializeField] private AudioClip intro;
+    [SerializeField] private AudioClip loop;
     void Awake()
     {
+        if(audioSource == null) audioSource = this.GetComponent<AudioSource>();
         if(Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -19,14 +22,23 @@ public class AudioManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
-
+    private void Start()
+    {
+        audioSource.clip = intro;
+        audioSource.Play();
+        audioSource.PlayScheduled(AudioSettings.dspTime + intro.length);
+        AudioSource loopSource = gameObject.AddComponent<AudioSource>();
+        loopSource.clip = loop;
+        loopSource.loop = true;
+        loopSource.PlayScheduled(AudioSettings.dspTime + intro.length);
+    }
     public void PlayTilePlacing()
     {
-        sfxSource.PlayOneShot(tilePlacing);
+        audioSource.PlayOneShot(tilePlacing);
     }
 
     public void PlayButtonClick()
     {
-        sfxSource.PlayOneShot(buttonClick);
+        audioSource.PlayOneShot(buttonClick);
     }
 }
