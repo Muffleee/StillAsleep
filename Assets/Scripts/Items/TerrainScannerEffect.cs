669a18fb235de531a28983aabda87142215d495d
Mulmer <steven.stark2@gmx.net>
2026-02-28 11:06:50 +0100

    add trap and scanner effects

---
+++ Assets/Scripts/Items/TerrainScannerEffect.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScannerEffect : MonoBehaviour
+{
+    [SerializeField] private float duration = 7f;
+    [SerializeField] private float size = 50f;
+    [SerializeField] private ParticleSystem terrainScanner;
+    public static TerrainScannerEffect INSTANCE;
+
+    void Awake()
+    {
+        INSTANCE = this;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.PlayTerrainScanner(3, 0.2f);
+        }
+    }
+
+    /// <summary>
+    /// Play the terrain scanner repetitions times with delay ms between scans 
+    /// </summary>
+    /// <param name="repetitions"></param>
+    /// <param name="delay"></param>
+    public void PlayTerrainScanner(int repetitions, float delay)
+    {
+        if(repetitions < 1) repetitions = 1;
+        for(int i = 1; i <= repetitions; i++)
+        {
+            this.Invoke(nameof(PlayTerrainScanner), delay * (i - 1));
+        }
+    }
+
+    /// <summary>
+    /// play the terrain scanner once
+    /// </summary>
+    private void PlayTerrainScanner()
+    {
+        var main = terrainScanner.main;
+        main.startLifetime = duration;
+        main.startSize = size;
+
+        terrainScanner.Play();
+    }
+}
