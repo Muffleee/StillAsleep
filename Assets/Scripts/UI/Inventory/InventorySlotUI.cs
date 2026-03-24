bcd3c5674051194bda93f0004d3df8230bbbcb01
Babachu38 <ngbabachu@gmail.com>
2026-03-24 02:45:45 +0100

    Added EnergyCost

--- Assets/Scripts/UI/Inventory/InventorySlotUI.cs
+++ Assets/Scripts/UI/Inventory/InventorySlotUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI; 
 
@@ -6,6 +7,8 @@ public class InventorySlotUI : MonoBehaviour
     [SerializeField] private Image iconImage;
     [Tooltip("Drag the new Highlight child object here")]
     [SerializeField] private GameObject highlightObject; 
+    [SerializeField] private GameObject costDiamond;
+    [SerializeField] private TMP_Text costText;
     
     public void Init()
     {
@@ -20,6 +23,12 @@ public class InventorySlotUI : MonoBehaviour
             iconImage.sprite = item.GetIcon();
             iconImage.enabled = true;
             iconImage.preserveAspect = true; 
+
+            if (costDiamond != null && costText != null)
+            {
+                costText.text = item.GetEnergyCost().ToString();
+                costDiamond.SetActive(true);
+            }
         }
         else
         {
@@ -31,6 +40,7 @@ public class InventorySlotUI : MonoBehaviour
     {
         iconImage.sprite = null;
         iconImage.enabled = false;
+        costDiamond.SetActive(false);
     }
     public void SetHighlight(bool isHighlighted)
     {
